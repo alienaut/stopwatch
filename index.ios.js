94a31a6fbde73863eaa36df96d6062def4e1d11b
Yusuf YALIM <yusufyalim@gmail.com>
2015-11-12 16:31:18 +0200

    Add structor and basic styling for app

--- index.ios.js
+++ index.ios.js
@@ -1,27 +1,54 @@
 'use strict';
 
 var React = require('react-native');
+var formatTime = require('minutes-seconds-milliseconds');
 
-var { AppRegistry, Text, View } = React;
+var { AppRegistry, Text, View, StyleSheet, TouchableHighlight } = React;
 
 var StopWatch = React.createClass({
+  getInitialState: function() {
+    return {
+      timeElapsed: null
+    };
+  },
   render: function() {
     return (
-      <View>
-        <Text> Hello world! </Text>
-        {this.startStopButton()}
-        {this.lapButton()}
+      <View style={styles.container}>
+        <View style={[styles.header, this.border('yellow')]} /* yellow */ >
+          <View  style={[this.border('red'), styles.timerWrapper]} /* red */ >
+            <Text> {formatTime(this.state.timeElapsed)} </Text>
+          </View>
+          <View style={[this.border('green'), styles.buttonWrapper]} /* green */ >
+            {this.startStopButton()}
+            {this.lapButton()}
+          </View>
+        </View>
+
+        <View style={[styles.footer, this.border('blue')]} /* blue */ >
+          <Text> list of laps </Text>
+        </View>
       </View>
     );
   },
   startStopButton: function() {
     return (
-      <View>
+      <TouchableHighlight
+        underlayColor="gray"
+        onPress={this.handleStartPress}
+      >
         <Text>
           Start
         </Text>
-      </View>
+      </TouchableHighlight>
     );
+  },
+  handleStartPress: function() {
+    var startTime = new Date();
+    setInterval(() => {
+      this.setState({
+        timeElapsed: new Date() - startTime
+      });
+    }, 30);
   },
   lapButton: function() {
     return (
@@ -31,6 +58,36 @@ var StopWatch = React.createClass({
         </Text>
       </View>
     );
+  },
+  border: function(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    };
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+  header: { // Yellow
+    flex: 1
+  },
+  footer: { // Blue
+    flex: 1
+  },
+  timerWrapper: { // Red
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: { // Green
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   }
 });
 
